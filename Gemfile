fc0412ff32db98b77fbe05be551ce056fb993035
Andres Gualde <andres@gualde.com>
2022-02-15 17:06:23 +0100

    Wikinonimus

--- Gemfile
+++ Gemfile
@@ -51,3 +51,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Faker gem
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+Article.create(
+  title: Faker::Book.title,
+  content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+)
+end
